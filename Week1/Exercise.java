7d55575d9823607cdce893d64a6f0840cce3164b
fl-sll <edwardalvin@gmail.com>
2023-02-15 21:34:19 +0700

    added exercise

---
+++ Week1/Exercise.java
@@ -0,0 +1,11 @@
+package Week1;
+
+public class Exercise {
+    // Make a java class for a car that has maxspeed, name, yearofmake
+
+    // Make a constructor to determine the maxspeed, name, yearofmake
+
+    // Make at least 2 methods
+
+    // Make a main function to define the object and its functions
+}
